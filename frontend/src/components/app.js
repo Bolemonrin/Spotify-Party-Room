88a5dbdb42014a9378ff1d421112e09ebb6b469d
Cookie Biscuit <oo222515@gmail.com>
2026-07-19 09:21:58 -0500

    made small changes

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,11 +1,7 @@
 import React, { Component } from "react";
-import { render } from "react-dom";
+import { createRoot } from "react-dom/client";
 
 export default class App extends Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return <h1>Testing React Code</h1>;
   }
