c58fdefc418becaadb8db14401137cf0eecd04be
Bolu <87003907+AfricanBolu@users.noreply.github.com>
2026-07-19 23:08:03 -0500

    fixed small issues

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -12,8 +12,13 @@ export default defineConfig({
   server: {
     port: 3000,
     proxy: {
+      // Django runserver defaults to 127.0.0.1:8000
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: true,
+      },
+      '/spotify': {
+        target: 'http://127.0.0.1:8000',
         changeOrigin: true,
       },
     },
